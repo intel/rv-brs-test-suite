f88eafeecedcb8b794254197443907849e8a214a
Stuart Yoder <stuart.yoder@arm.com>
2021-08-18 21:33:43 -0500

    security-extension: generate signed siglists for secure boot keys
    to enable secure boot in u-boot we need signed signature lists
    
    Signed-off-by: Stuart Yoder <stuart.yoder@arm.com>

--- common/scripts/build-security-extension-acs-keys.sh
+++ common/scripts/build-security-extension-acs-keys.sh
@@ -37,35 +37,47 @@
 TOP_DIR=`pwd`
 KEYS_DIR=$TOP_DIR/security-extension-acs-keys
 
+# set the path to pick up the local efitools
+export PATH="$TOP_DIR/efitools:$PATH"
+
 do_build()
 {
     echo "do_build: security-extension-acs-keys"
     mkdir -p $KEYS_DIR
     pushd $KEYS_DIR
 
-    # generate TestPK1
+    # generate TestPK1: DER and signed siglist
     NAME=TestPK1
     openssl req -x509 -sha256 -newkey rsa:2048 -subj /CN=TEST_PK/  -keyout $NAME.key -out $NAME.crt -nodes -days 4000
     openssl x509 -outform der -in $NAME.crt -out $NAME.der
+    cert-to-efi-sig-list $NAME.crt $NAME.esl
+    sign-efi-sig-list -c $NAME.crt -k $NAME.key PK $NAME.esl $NAME.auth
 
-    # generate TestKEK1
+    # generate TestKEK1: DER and signed siglist
     NAME=TestKEK1
     openssl req -x509 -sha256 -newkey rsa:2048 -subj /CN=TEST_KEK/  -keyout $NAME.key -out $NAME.crt -nodes -days 4000
     openssl x509 -outform der -in $NAME.crt -out $NAME.der
+    cert-to-efi-sig-list $NAME.crt $NAME.esl
+    sign-efi-sig-list -c TestPK1.crt -k TestPK1.key KEK $NAME.esl $NAME.auth
 
-    # generate TestDB1
+    # generate TestDB1: DER and signed siglist
     NAME=TestDB1
     openssl req -x509 -sha256 -newkey rsa:2048 -subj /CN=TEST_DB/  -keyout $NAME.key -out $NAME.crt -nodes -days 4000
     openssl x509 -outform der -in $NAME.crt -out $NAME.der
+    cert-to-efi-sig-list $NAME.crt $NAME.esl
+    sign-efi-sig-list -c TestKEK1.crt -k TestKEK1.key db $NAME.esl $NAME.auth
+
     # Convert TestDB1 to gpg form and import to gpg toolchain for use in signing grub
     cat TestDB1.key | PEM2OPENPGP_USAGE_FLAGS=certify,sign pem2openpgp "TestDB1"  > TestDB1.gpgkey
     gpg --import --allow-secret-key-import TestDB1.gpgkey
     gpg --export > TestDB1.pubgpg
 
-    # generate TestDBX1
+    # generate TestDBX1: DER and signed siglist
     NAME=TestDBX1
     openssl req -x509 -sha256 -newkey rsa:2048 -subj /CN=TEST_PK/  -keyout $NAME.key -out $NAME.crt -nodes -days 4000
     openssl x509 -outform der -in $NAME.crt -out $NAME.der
+    cert-to-efi-sig-list $NAME.crt $NAME.esl
+    sign-efi-sig-list -c TestKEK1.crt -k TestKEK1.key dbx $NAME.esl $NAME.auth
 
     popd
 }
