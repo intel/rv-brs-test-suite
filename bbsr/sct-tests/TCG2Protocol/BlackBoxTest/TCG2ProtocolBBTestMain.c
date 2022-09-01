dd06fe165ea3685ba22c9bb7e3cf36a60ec99186
Amrathesh <amrathesh@arm.com>
2022-09-01 14:08:54 +0530

    fix inconsistent line endings
    Signed-off-by: Amrathesh <amrathesh@arm.com>

--- bbsr/sct-tests/TCG2Protocol/BlackBoxTest/TCG2ProtocolBBTestMain.c
+++ bbsr/sct-tests/TCG2Protocol/BlackBoxTest/TCG2ProtocolBBTestMain.c
@@ -2,7 +2,8 @@
 
   Copyright 2006 - 2017 Unified EFI, Inc.<BR>
   Copyright (c) 2013, Intel Corporation. All rights reserved.<BR>
-  Copyright (c) 2021, Arm Inc. All rights reserved.<BR>
+  Copyright (c) 2021, Arm Inc. All rights reserved.<BR>
+
 
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
